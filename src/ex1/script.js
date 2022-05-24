432c72b2889e9b17e93bb01403caa7add8f7188a
Amit Lago <amitlago@Amits-MacBook-Pro.local>
2022-05-24 14:16:27 +0300

    refactor(project): address review comments and clean code

--- src/ex1/script.js
+++ src/ex1/script.js
@@ -1,45 +1,56 @@
-const inputBox = document.querySelector(".newTodoInput");
-const addButton = document.querySelector(".addTodoButton");
+const inputBox = document.querySelector(".new-to-do-input");
+const addButton = document.querySelector(".add-to-do-button");
 const todoList = document.querySelector(".todo-list");
-const deleteAllButton = document.querySelector(".deleteAllButton");
+const deleteAllButton = document.querySelector(".delete-all-button");
+const localStorageName = "New Todo";
 
-showTasks();
+renderToDoList();
 
-inputBox.onkeyup = () => {
-    let userData = inputBox.value;
-    if (userData.trim()!=0){
+inputBox.addEventListener("keyup", function(){
+    const userData = inputBox.value;
+    if (userData.trim()!==0){
         addButton.classList.add("active");
     }
     else{
         addButton.classList.remove("active");
     }
+});
+
+// inputBox.onkeyup = () => {
+//     let userData = inputBox.value;
+//     if (userData.trim()!==0){
+//         addButton.classList.add("active");
+//     }
+//     else{
+//         addButton.classList.remove("active");
+//     }
+// }
+
+
+function checkIfTasksExist() {
+    const getLocalStorage = localStorage.getItem(localStorageName);
+    if(getLocalStorage === null){
+        listArr = [];
+    }
+    else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    return listArr;
 }
 
 addButton.addEventListener("click",function(){
-    let userData = inputBox.value;
-    let getLocalStorage = localStorage.getItem("New Todo");
-    if(getLocalStorage == null){
-        listArr = [];
-    }
-    else{
-        listArr = JSON.parse(getLocalStorage);
-    }
+    const userData = inputBox.value;
+    let listArr = checkIfTasksExist();
     listArr.push(userData);
-    localStorage.setItem("New Todo", JSON.stringify(listArr));
-    showTasks();
+    localStorage.setItem(localStorageName, JSON.stringify(listArr));
+    renderToDoList();
     addButton.classList.remove("active");
+    inputBox.value='';
 });
 
-function showTasks(){
-    let getLocalStorage = localStorage.getItem("New Todo");
-
-    if(getLocalStorage == null){
-        listArr = [];
-    }
-    else{
-        listArr = JSON.parse(getLocalStorage);
-    }
-    const pendingTasksCounter = document.querySelector(".pendingNum");
+function renderToDoList(){
+    let listArr = checkIfTasksExist();
+    const pendingTasksCounter = document.querySelector(".pending-num");
     pendingTasksCounter.textContent = listArr.length;
     if (listArr.length > 0) {
         deleteAllButton.classList.add("active");
@@ -52,19 +63,18 @@ function showTasks(){
         newListItem +=`<li> ${element} <span onclick="deleteTask(${index})"; ><i class="fas fa-trash"></i></span></li>`;
     });
     todoList.innerHTML = newListItem;
-    inputBox.value='';
 }
 
 function deleteTask(index){
-    let getLocalStorage = localStorage.getItem("New Todo");
+    const getLocalStorage = localStorage.getItem(localStorageName);
     listArr = JSON.parse(getLocalStorage);
     listArr.splice(index, 1);
-    localStorage.setItem("New Todo", JSON.stringify(listArr));
-    showTasks();
+    localStorage.setItem(localStorageName, JSON.stringify(listArr));
+    renderToDoList();
 }
 
 deleteAllButton.addEventListener("click", function(){
     listArr=[];
-    localStorage.setItem("New Todo", JSON.stringify(listArr));
-    showTasks();
+    localStorage.setItem(localStorageName, JSON.stringify(listArr));
+    renderToDoList();
 });
